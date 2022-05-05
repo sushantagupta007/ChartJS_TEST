3b193ca97e62ca2b0632a728d55c412a61a163fe
Sushanta Gupta <sushanta.gupta007@gmail.com>
2022-05-06 00:09:49 +0600

    Chart JS ADD

---
+++ chart-js/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import BarChart from './BarChart/BarChart';
+
+function App() {
+  return (
+    <div className="App">
+      <BarChart></BarChart>
+    </div>
+  );
+}
+
+export default App;
---
+++ chart-js/src/BarChart/BarChart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BarChart = () => {
+  return (
+    <div>
+      <h1> BarChart</h1>
+    </div>
+  );
+};
+
+export default BarChart;
